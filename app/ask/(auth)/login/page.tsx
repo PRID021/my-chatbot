8e35d3a5de03ba44eeac1c4529ea82c40ea23e8c
hoang.pham <hoang.pham@executionlab.asia>
2024-11-09 13:52:19 +0700

    styling

--- app/ask/(auth)/login/page.tsx
+++ app/ask/(auth)/login/page.tsx
@@ -38,12 +38,12 @@ export default function Page() {
   };
 
   return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-gray-100 dark:bg-gray-900 p-4 sm:p-8 lg:p-16">
+    <div className="w-screen md:h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 p-4 sm:p-8 lg:p-16">
       <div
         className="flex flex-col-reverse items-end justify-between gap-2 rounded-lg bg-white dark:bg-gray-800 p-8 shadow-lg 
-                  md:flex-row md:gap-4 md:p-12 lg:gap-8 lg:p-16"
+              md:flex-row md:gap-4 md:p-12 lg:gap-8 lg:p-16 h-full"
       >
-        <div className="flex flex-col items-center text-center w-full md:items-center md:text-left md:w-1/2">
+        <div className="flex flex-col size-full items-center md:justify-between text-center md:items-center md:text-left md:w-1/2">
           <div className="w-full md:w-1/2 mb-4 hidden md:flex items-center justify-center">
             <Image
               src="/images/ask-icon.svg"
@@ -55,7 +55,7 @@ export default function Page() {
           <h3 className="text-2xl font-bold text-gray-800 dark:text-gray-200 my-4 self-center md:self-start">
             Sign In
           </h3>
-          <h4 className="text-sm text-gray-600 dark:text-gray-400 mb-6 font-bold self-start">
+          <h4 className="text-sm text-gray-600 dark:text-gray-400 font-bold self-start">
             Sign in with your email and password to explore all available
             features.
           </h4>
@@ -64,7 +64,7 @@ export default function Page() {
             <SubmitButton>Sign in</SubmitButton>
           </AuthForm>
 
-          <div className="flex items-center justify-center w-full mt-4">
+          <div className="flex items-center justify-center w-full">
             <hr className="grow border-t border-gray-300 bg-gradient-to-r from-transparent via-gray-300 to-transparent" />
             <span className="mx-4 text-gray-500 dark:text-gray-400 font-medium text-base">
               or
@@ -72,7 +72,7 @@ export default function Page() {
             <hr className="grow border-t border-gray-300 bg-gradient-to-r from-transparent via-gray-300 to-transparent" />
           </div>
 
-          <p className="text-sm text-gray-600 dark:text-gray-400 mt-2 lg:mb-12 text-center self-center">
+          <p className="text-sm text-gray-600 dark:text-gray-400 mt-2 text-center self-center">
             {"Don't have an account? "}
             <Link
               href="/ask/register"
@@ -84,12 +84,13 @@ export default function Page() {
           </p>
         </div>
 
-        <div className="w-full md:w-1/2  justify-center items-center my-auto">
+        <div className="size-full md:w-1/2 flex justify-center items-center ">
           <Image
             src="/images/illustration.svg"
             alt="Illustration"
             width={600}
             height={400}
+            style={{ width: "100%", height: "auto" }}
           />
         </div>
       </div>
